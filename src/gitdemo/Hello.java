6237bd7d030379c5c899185d2aa3f746b6d3f544
sdzb <1184659263@qq.com>
2017-08-09 06:46:12 +0800

    添加内容

---
+++ src/gitdemo/Hello.java
@@ -0,0 +1,16 @@
+package gitdemo;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		
+		
+		System.out.println("Git is runnning !");
+		
+		
+		
+
+	}
+
+}
